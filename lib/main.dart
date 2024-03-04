179d2f3c187f85a89e63851d56ca5811790324d7
cantikawijaya <cantikawjy@gmail.com>
2024-03-04 15:30:30 +0800

    Part D mengisi body dengan komponen row

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,13 @@ class HomePage extends StatelessWidget{
       appBar: AppBar(
         title: Text('Praktikum Layout Flutter'),
       ),
-      body: 
+      body: Row(
+        children: <Widget>[
+          Container(color: Color.fromARGB(255, 241, 57, 195), height: 100, width: 100),
+          Container(color: Color.fromARGB(255, 138, 180, 114), height: 100, width: 100),
+          Container(color: Color.fromARGB(255, 225, 158, 228), height: 100, width: 100),
+        ]
+        )
     );
   }
 }
